ad5d4aebc41de0023cee59fe1b71e17eec0f3ee9
olamide1992 <mikaheelazeez192@gmail.com>
2022-01-04 13:23:37 -0800

    Inital Commit

---
+++ testing.js
@@ -0,0 +1,20 @@
+/**
+   * Navbar links active state on scroll
+  */ 
+let navbarlinks = select('#navbar .nav-link', true)
+const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+        if (!navbarlink.hash) return
+        let section = select(navbarlink.hash)
+        if (!section) return
+        if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+            navbarlink.classList.add('active')
+        }   else {
+            navbarlink.classList.remove('active')
+        }
+    })
+}
+window.addEventListener('load', navbarlinksActive)
+onscroll(document, navbarlinksActive)
+
